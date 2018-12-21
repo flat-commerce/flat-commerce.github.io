d99e6a500202f64f065d6136390dcf693ab5d1af
Quinten Clause <quinten.clause@studioemma.eu>
2018-12-21 14:40:31 +0100

    Add build

---
+++ build/precache-manifest.a7b766eb15b24e0c7e4617c3c4a77bec.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "562d602e0d3374f33d11",
+    "url": "/static/js/main.562d602e.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "562d602e0d3374f33d11",
+    "url": "/static/css/main.bc4c5c5d.chunk.css"
+  },
+  {
+    "revision": "de4f633a569d3785e757292b60df6432",
+    "url": "/index.html"
+  }
+];
