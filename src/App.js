ea781f079f01f21b09d945def5d6bca1d988e4d4
Quinten Clause <quinten.clause@studioemma.eu>
2018-12-30 20:47:48 +0100

    Tweak stuff

--- src/App.js
+++ src/App.js
@@ -6,8 +6,11 @@ class App extends Component {
         return (
             <div className="App">
                 <header className="App-header">
-                    <h1 className="App-logo"><span>S</span></h1>
-                    <p>Flat Commerce</p>
+                    <div className="App-bg"></div>
+                    <div className="App-fg">
+                        <h1 className="App-logo"><span>S</span></h1>
+                        <p>Flat Commerce</p>
+                    </div>
                 </header>
             </div>
         );
